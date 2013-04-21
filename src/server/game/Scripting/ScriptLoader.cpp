73ddbfd1d56c9cd2f22b843b34ae90e3771be068
Zachary MacPhee <lhorath94@gmail.com>
2013-04-20 22:34:32 -0300

    Transmogrification Added to Server
    Thank You Rochet2!

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -1273,6 +1273,7 @@ void AddBattlegroundScripts()
 #ifdef SCRIPTS
 /* This is where custom scripts' loading functions should be declared. */
 
+void AddSC_NPC_Transmogrify();
 #endif
 
 void AddCustomScripts()
@@ -1280,5 +1281,6 @@ void AddCustomScripts()
 #ifdef SCRIPTS
     /* This is where custom scripts should be added. */
 
+    AddSC_NPC_Transmogrify();
 #endif
 }
